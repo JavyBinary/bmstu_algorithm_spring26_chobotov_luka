aafc7736472bdeebbf246559d4c8bddb7089f8bc
Luka <lucachobo@gmail.com>
2026-03-23 20:28:39 +0300

    final 01

--- 01/Intersection_of_two_lists.py
+++ 01/Intersection_of_two_lists.py
@@ -9,17 +9,17 @@ def find_intersection_of_two_lists(head_A: Node, head_B: Node) -> Node:
         return None
 
     current_node_A = head_A
-    current_bode_B = head_B
+    current_node_B = head_B
 
-    while current_node_A is not current_bode_B:
+    while current_node_A is not current_node_B:
         if not current_node_A:
             current_node_A = head_B
         else:
             current_node_A = current_node_A.next
 
-        if not current_bode_B:
-            current_bode_B = head_B
+        if not current_node_B:
+            current_node_B = head_A
         else:
-            current_bode_B = current_bode_B.next
+            current_node_B = current_node_B.next
 
     return current_node_A
---
+++ 01/test_Intersection_of_two_lists.py
@@ -0,0 +1,19 @@
+from Intersection_of_two_lists import find_intersection_of_two_lists, Node
+
+def test_find_intersection_of_two_lists():
+    intersection_node = Node(8)
+    intersection_node.next = Node(4)
+    intersection_node.next.next = Node(5)
+    
+    list_A = Node(4)
+    list_A.next = Node(1)
+    list_A.next.next = intersection_node
+
+    list_B = Node(5)
+    list_B.next = Node(6)
+    list_B.next.next = Node(1)
+    list_B.next.next.next = intersection_node
+
+    assert find_intersection_of_two_lists(list_A, list_B) is intersection_node
+
+test_find_intersection_of_two_lists() 
